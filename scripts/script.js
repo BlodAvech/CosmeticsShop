e724bdbc925200e3fbbd8313942bf51bb1726ed6
Blod <erkhanpiriyev@gmail.com>
2025-10-13 15:38:43 +0500

    отдделил script 2 логики

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,3 @@
-const amountInputs = document.querySelectorAll('input[name="cart_item_amount"]');
-const prices = document.querySelectorAll('.cart_item_price');
-const totalPrice = document.querySelector('#cart_totalprice');
-const removeButtons = document.querySelectorAll('.cart_item_remove');
-
-
-function OnCartItemAmountChanged()
-{
-	let sum = 0;
-
-	for(let i = 0 ; i < amountInputs.length ; i++)
-	{
-		sum += Math.max(+amountInputs[i].value , 0) * +prices[i].textContent;
-	}
-
-	totalPrice.textContent = sum;
-}
-
-removeButtons.forEach((button , index) => {
-	button.addEventListener('click' , () => {
-		amountInputs[index].value = 0;
-		OnCartItemAmountChanged();
-	});
-});
-
-amountInputs.forEach((input , index) => {
-	input.addEventListener('input',() => OnCartItemAmountChanged());
-});
-
-//Form
 const passwordMinLenth = 8;
 const forms = document.querySelectorAll('form');
 const namePattern = /^[A-Za-z]+$/;
