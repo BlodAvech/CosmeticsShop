484641a96f7618f1413ceaf482f26fb61ea6276e
Saltanat4 <242242@astanait.edu.kz>
2025-10-27 19:44:10 +0500

    clean up2

--- scripts/register.js
+++
@@ -1,9 +0,0 @@
-// Registration popup
-document.querySelector(".open_registration_btn").onclick=function(){
-    document.querySelector(".registration").style.display="block";
-    document.querySelector(".close_registration_btn").style.display="block";
-}
-
-document.querySelector(".close_registration_btn").onclick=function(){
-    document.querySelector(".registration").style.display="none";
-}
